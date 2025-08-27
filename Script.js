39169700b93c4bd0cfcd965313c429f907ce532a
shakiburrashid <shakiburrashid7@gmail.com>
2025-08-28 02:10:09 +0600

    In card item, love icon is click then is increase love

--- Script.js
+++ Script.js
@@ -1,6 +1,9 @@
+
+// ! Love function
 const loveIcon =  document.querySelectorAll('.love-count');
+let b = 3;
 for(const i of loveIcon){
-   i.addEventListener('click',()=>{
-    // console.log("click");
+    i.addEventListener('click',()=>{
+    document.getElementById("love-number").innerText = ++b;
    })
 }
