51f30b0fdc1d1f58f0d5f4a0ec71e7d96f65dbc7
shakiburrashid <shakiburrashid7@gmail.com>
2025-08-28 02:44:31 +0600

    For Call reduce start function created

--- Script.js
+++ Script.js
@@ -1,9 +1,31 @@
 
-// ! Love function
+// ! Love Matter
+
 const loveIcon =  document.querySelectorAll('.love-count');
-let b = 3;
+let a = 0;
 for(const i of loveIcon){
     i.addEventListener('click',()=>{
-    document.getElementById("love-number").innerText = ++b;
+    document.getElementById("love-number").innerText = ++a;
    })
+}
+
+//! Call Matter
+const callCount = document.querySelectorAll(".call-count");
+let b = 100;
+for(const y of callCount){
+    y.addEventListener("click",()=>{
+
+       if(b>0){
+         b = b - 20
+         document.getElementById("star-number").innerText = b
+       }
+       
+        if(b > 20){
+            alert("Calling Now")
+        } else{
+            alert("You haven't sufficient star, minimum you have to need 20 star for call !");
+        }
+        
+        
+    })
 }
