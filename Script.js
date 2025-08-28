7d33e0ba7e735e5780123cea7bf94c28d7b7be38
shakiburrashid <shakiburrashid7@gmail.com>
2025-08-29 01:51:13 +0600

    Call history created

--- Script.js
+++ Script.js
@@ -1,20 +1,43 @@
 
 // ! Love Section
-{
-    const loveIcon = document.querySelectorAll('.love-count');
-    let a = 0;
-    for (const i of loveIcon) {
-        i.addEventListener('click', () => {
-            document.getElementById("love-number").innerText = ++a;
-        })
-    }
 
-
-
+const loveIcon = document.querySelectorAll('.love-count');
+let a = 0;
+for (const i of loveIcon) {
+    i.addEventListener('click', () => {
+        document.getElementById("love-number").innerText = ++a;
+    })
 }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //! Call Section
+
+const date = new Date();
+let nowTime = date.toLocaleTimeString();
+// console.log(nowTime);
+
+
+
+const serviceTitle = document.querySelectorAll(".service-title");
+let serviceTitleList = [];
+for (const z of serviceTitle) {
+    serviceTitleList.push(z.innerText);
+}
+
 
 const serviceName = document.querySelectorAll('.service-name');
 const listofservice = [];
@@ -34,9 +57,24 @@ function callOne() {
     if (b >= 0) {
         alert(`📞 Calling Now ... Service Name: ${listofservice[0]} , Service Number: ${listofnumber[0]} `)
         document.getElementById("star-number").innerText = b;
+
+        const historyList = document.getElementById("history-list");
+        const div = document.createElement('div')
+        div.innerHTML =`
+         <div class="flex justify-between items-center p-4 rounded-sm bg-gray-50 ">
+                        <div>
+                            <p class="font-bold">${serviceTitleList[0]}</p>
+                            <p class="text-gray-500">${listofnumber[0]}</p>
+                        </div>
+                        <p>${nowTime}</p>
+                    </div>
+        `
+        historyList.appendChild(div);
+
     } else {
         alert("❌ You haven't sufficient star, minimum you have to need 20 star for call !");
     }
+
 }
 
 function callTwo() {
@@ -44,6 +82,18 @@ function callTwo() {
     if (b >= 0) {
         alert(`📞 Calling Now ... Service Name: ${listofservice[1]} , Service Number: ${listofnumber[1]} `)
         document.getElementById("star-number").innerText = b;
+        const historyList = document.getElementById("history-list");
+        const div = document.createElement('div')
+        div.innerHTML =`
+         <div class="flex justify-between items-center p-4 rounded-sm bg-gray-50 ">
+                        <div>
+                            <p class="font-bold">${serviceTitleList[1]}</p>
+                            <p class="text-gray-500">${listofnumber[1]}</p>
+                        </div>
+                        <p>${nowTime}</p>
+                    </div>
+        `
+        historyList.appendChild(div);
     } else {
         alert("❌ You haven't sufficient star, minimum you have to need 20 star for call !");
     }
@@ -53,6 +103,18 @@ function callThree() {
     if (b >= 0) {
         alert(`📞 Calling Now... Service Name: ${listofservice[2]} , Service Number: ${listofnumber[2]} `)
         document.getElementById("star-number").innerText = b;
+        const historyList = document.getElementById("history-list");
+        const div = document.createElement('div')
+        div.innerHTML =`
+         <div class="flex justify-between items-center p-4 rounded-sm bg-gray-50 ">
+                        <div>
+                            <p class="font-bold">${serviceTitleList[2]}</p>
+                            <p class="text-gray-500">${listofnumber[2]}</p>
+                        </div>
+                        <p>${nowTime}</p>
+                    </div>
+        `
+        historyList.appendChild(div);
     } else {
         alert("❌ You haven't sufficient star, minimum you have to need 20 star for call !");
     }
@@ -62,6 +124,18 @@ function callFour() {
     if (b >= 0) {
         alert(`📞 Calling Now ... Service Name: ${listofservice[3]} , Service Number: ${listofnumber[3]} `)
         document.getElementById("star-number").innerText = b;
+        const historyList = document.getElementById("history-list");
+        const div = document.createElement('div')
+        div.innerHTML =`
+         <div class="flex justify-between items-center p-4 rounded-sm bg-gray-50 ">
+                        <div>
+                            <p class="font-bold">${serviceTitleList[3]}</p>
+                            <p class="text-gray-500">${listofnumber[3]}</p>
+                        </div>
+                        <p>${nowTime}</p>
+                    </div>
+        `
+        historyList.appendChild(div);
     } else {
         alert("❌ You haven't sufficient star, minimum you have to need 20 star for call !");
     }
@@ -71,6 +145,18 @@ function callFive() {
     if (b >= 0) {
         alert(`📞 Calling Now ... Service Name: ${listofservice[4]} , Service Number: ${listofnumber[4]} `)
         document.getElementById("star-number").innerText = b;
+        const historyList = document.getElementById("history-list");
+        const div = document.createElement('div')
+        div.innerHTML =`
+         <div class="flex justify-between items-center p-4 rounded-sm bg-gray-50 ">
+                        <div>
+                            <p class="font-bold">${serviceTitleList[4]}</p>
+                            <p class="text-gray-500">${listofnumber[4]}</p>
+                        </div>
+                        <p>${nowTime}</p>
+                    </div>
+        `
+        historyList.appendChild(div);
     } else {
         alert("❌ You haven't sufficient star, minimum you have to need 20 star for call !");
     }
@@ -80,6 +166,18 @@ function callSix() {
     if (b >= 0) {
         alert(`📞 Calling Now ... Service Name: ${listofservice[5]} , Service Number: ${listofnumber[5]} `)
         document.getElementById("star-number").innerText = b;
+        const historyList = document.getElementById("history-list");
+        const div = document.createElement('div')
+        div.innerHTML =`
+         <div class="flex justify-between items-center p-4 rounded-sm bg-gray-50 ">
+                        <div>
+                            <p class="font-bold">${serviceTitleList[5]}</p>
+                            <p class="text-gray-500">${listofnumber[5]}</p>
+                        </div>
+                        <p>${nowTime}</p>
+                    </div>
+        `
+        historyList.appendChild(div);
     } else {
         alert("❌ You haven't sufficient star, minimum you have to need 20 star for call !");
     }
@@ -89,6 +187,18 @@ function callSeven() {
     if (b >= 0) {
         alert(`📞 Calling Now ... Service Name: ${listofservice[6]} , Service Number: ${listofnumber[6]} `)
         document.getElementById("star-number").innerText = b;
+        const historyList = document.getElementById("history-list");
+        const div = document.createElement('div')
+        div.innerHTML =`
+         <div class="flex justify-between items-center p-4 rounded-sm bg-gray-50 ">
+                        <div>
+                            <p class="font-bold">${serviceTitleList[6]}</p>
+                            <p class="text-gray-500">${listofnumber[6]}</p>
+                        </div>
+                        <p>${nowTime}</p>
+                    </div>
+        `
+        historyList.appendChild(div);
     } else {
         alert("❌ You haven't sufficient star, minimum you have to need 20 star for call !");
     }
@@ -98,6 +208,18 @@ function callEight() {
     if (b >= 0) {
         alert(`📞 Calling Now ... Service Name: ${listofservice[7]} , Service Number: ${listofnumber[7]} `)
         document.getElementById("star-number").innerText = b;
+        const historyList = document.getElementById("history-list");
+        const div = document.createElement('div')
+        div.innerHTML =`
+         <div class="flex justify-between items-center p-4 rounded-sm bg-gray-50 ">
+                        <div>
+                            <p class="font-bold">${serviceTitleList[7]}</p>
+                            <p class="text-gray-500">${listofnumber[7]}</p>
+                        </div>
+                        <p>${nowTime}</p>
+                    </div>
+        `
+        historyList.appendChild(div);
     } else {
         alert("❌ You haven't sufficient star, minimum you have to need 20 star for call !");
     }
@@ -107,6 +229,18 @@ function callNine() {
     if (b >= 0) {
         alert(`📞 Calling Now ... Service Name: ${listofservice[8]} , Service Number: ${listofnumber[8]} `)
         document.getElementById("star-number").innerText = b;
+        const historyList = document.getElementById("history-list");
+        const div = document.createElement('div')
+        div.innerHTML =`
+         <div class="flex justify-between items-center p-4 rounded-sm bg-gray-50 ">
+                        <div>
+                            <p class="font-bold">${serviceTitleList[8]}</p>
+                            <p class="text-gray-500">${listofnumber[8]}</p>
+                        </div>
+                        <p>${nowTime}</p>
+                    </div>
+        `
+        historyList.appendChild(div)
     } else {
         alert("❌ You haven't sufficient star, minimum you have to need 20 star for call !");
     }
@@ -186,16 +320,4 @@ function copyNine() {
     document.getElementById("copy-count").innerText = copyNumberNow++;
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
 
