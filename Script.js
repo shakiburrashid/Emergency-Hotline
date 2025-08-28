45c3243c17a244bf9eedd0bc2a92da1fe3e4ac47
shakiburrashid <shakiburrashid7@gmail.com>
2025-08-28 12:19:45 +0600

    Call section problem solve

--- Script.js
+++ Script.js
@@ -1,31 +1,31 @@
 
 // ! Love Matter
 
-const loveIcon =  document.querySelectorAll('.love-count');
+const loveIcon = document.querySelectorAll('.love-count');
 let a = 0;
-for(const i of loveIcon){
-    i.addEventListener('click',()=>{
-    document.getElementById("love-number").innerText = ++a;
-   })
+for (const i of loveIcon) {
+    i.addEventListener('click', () => {
+        document.getElementById("love-number").innerText = ++a;
+    })
 }
 
 //! Call Matter
+
 const callCount = document.querySelectorAll(".call-count");
 let b = 100;
-for(const y of callCount){
-    y.addEventListener("click",()=>{
-
-       if(b>0){
-         b = b - 20
-         document.getElementById("star-number").innerText = b
-       }
-       
-        if(b > 20){
-            alert("Calling Now")
-        } else{
-            alert("You haven't sufficient star, minimum you have to need 20 star for call !");
+for (const y of callCount) {
+    y.addEventListener("click", () => {
+        b = b - 20;
+        console.log(b);
+        if (b >= 0) {
+            alert(`📞 Calling Now ... Service Name: , Service Number: ${b} `)
+            document.getElementById("star-number").innerText = b;
+        } else {
+            alert("❌ You haven't sufficient star, minimum you have to need 20 star for call !");
         }
-        
-        
+
     })
 }
+
+//! Copy Button Matter
+//number-copy
